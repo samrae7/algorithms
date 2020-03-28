da8e760c361eb953e8d02bec50f8a1725d4590b9
Sam Rae <samuelrae@gmail.com>
2020-03-28 19:26:46 +0000

    make it work for unordered list

--- src/com/company/LinkedListAnswers.java
+++ src/com/company/LinkedListAnswers.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Hashtable;
+
 public class LinkedListAnswers {
-    public static Node removeDups(Node head) {
+    public static Node removeDuplicates(Node head) {
         Node n = head;
+        Hashtable<Integer, Integer> uniqueValues = new Hashtable<>(2, 1);
         while(n.next != null) {
-            if(n.data == n.next.data) {
+            uniqueValues.put(n.data, n.data);
+            if(uniqueValues.contains(n.next.data)) {
                 n.next = n.next.next;
             }
             n = n.next;
--- src/com/company/LinkedListAnswersTest.java
+++ src/com/company/LinkedListAnswersTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class LinkedListAnswersTest {
     @Test
-    public void shouldRemoveDups() {
+    public void shouldRemoveDuplicatess() {
         Node linkedList = new Node(1);
         linkedList.appendToTail(2);
         linkedList.appendToTail(2);
@@ -13,8 +13,22 @@ public class LinkedListAnswersTest {
         linkedList.appendToTail(4);
         int thirdItem = linkedList.next.next.data;
         Assert.assertEquals(thirdItem, 2);
-        LinkedListAnswers.removeDups(linkedList);
+        LinkedListAnswers.removeDuplicates(linkedList);
         int thirdItemAfterRemoveDups = linkedList.next.next.data;
         Assert.assertEquals(thirdItemAfterRemoveDups, 3);
+    }
+
+    @Test
+    public void shouldRemoveDuplicatesWhenUnordered() {
+        Node linkedList = new Node(4);
+        linkedList.appendToTail(2);
+        linkedList.appendToTail(3);
+        linkedList.appendToTail(2);
+        linkedList.appendToTail(1);
+        int fourthItem = linkedList.next.next.next.data;
+        Assert.assertEquals(fourthItem, 2);
+        LinkedListAnswers.removeDuplicates(linkedList);
+        int fouthItemAfterRemoveDups = linkedList.next.next.next.data;
+        Assert.assertEquals(fouthItemAfterRemoveDups, 1);
     }
 }
